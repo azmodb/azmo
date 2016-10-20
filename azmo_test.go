2e93d9c2fa21e58811d6e68ba25946a33d98146f
Markus Sonderegger <marraison@gmail.com>
2016-10-20 23:00:39 +0200

    improve basic test case

--- azmo_test.go
+++ azmo_test.go
@@ -1,11 +1,10 @@
 package azmo
 
 import (
+	"bytes"
 	"context"
 	"fmt"
-	"io"
 	"net"
-	"os"
 	"testing"
 
 	pb "github.com/azmodb/azmo/azmopb"
@@ -38,6 +37,7 @@ func init() {
 	listener = l
 }
 
+/*
 type fmtEncoder struct {
 	w io.Writer
 }
@@ -46,16 +46,44 @@ func (e fmtEncoder) Encode(ev *pb.Event) error {
 	_, err := fmt.Fprintln(e.w, ev)
 	return err
 }
+*/
 
-func TestBasicServerClient(t *testing.T) {
+type testEncoder struct {
+	typ     pb.Event_Type
+	key     []byte
+	content []byte
+	created int64
+	current int64
+}
+
+func (e testEncoder) Encode(ev *pb.Event) error {
+	if e.typ != ev.Type {
+		return fmt.Errorf("expected event type %v, have %v", e.typ, ev.Type)
+	}
+	if bytes.Compare(e.key, ev.Key) != 0 {
+		return fmt.Errorf("expected event key %v, have %v", e.key, ev.Key)
+	}
+	if bytes.Compare(e.content, ev.Content) != 0 {
+		return fmt.Errorf("expected event content %v, have %v", e.content, ev.Content)
+	}
+	if e.created != ev.Created {
+		return fmt.Errorf("expected created rev %d, have %d", e.created, ev.Created)
+	}
+	if e.current != ev.Current {
+		return fmt.Errorf("expected current rev %d, have %d", e.current, ev.Current)
+	}
+	return nil
+}
+
+func TestPutGet(t *testing.T) {
 	c, err := Dial(defaultTestServerAddress, 0)
 	if err != nil {
 		t.Fatalf("cannot dial server: %v", err)
 	}
 	defer c.Close()
 
-	e := fmtEncoder{w: os.Stdout}
-	err = c.Put(context.TODO(), e, &pb.PutRequest{
+	putTest := testEncoder{typ: pb.Put, key: []byte("k1"), created: 1, current: 1}
+	err = c.Put(context.TODO(), putTest, &pb.PutRequest{
 		Key:   []byte("k1"),
 		Value: []byte("v1"),
 	})
@@ -63,7 +91,8 @@ func TestBasicServerClient(t *testing.T) {
 		t.Fatalf("put k1: %v", err)
 	}
 
-	err = c.Get(context.TODO(), e, &pb.GetRequest{
+	getTest := testEncoder{typ: pb.Get, key: []byte("k1"), content: []byte("v1"), created: 1, current: 1}
+	err = c.Get(context.TODO(), getTest, &pb.GetRequest{
 		Key:       []byte("k1"),
 		Revision:  0,
 		MustEqual: false,
